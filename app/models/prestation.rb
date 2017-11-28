51500b714e949997a2ce578ccfd4af89e227013e
Alix de Bricourt <alix.debricourt@gmail.com>
2017-11-28 18:06:19 +0100

    Police, couleur, bouton, index, cards

--- app/models/prestation.rb
+++ app/models/prestation.rb
@@ -1,4 +1,9 @@
 class Prestation < ApplicationRecord
+  gradiant_red = 'linear-gradient(-90deg, #FF0108, #FF4F61);'
+  gradiant_blue = 'linear-gradient(-90deg, #2ED2D7, #26A8D5);'
+  gradiant_green = 'linear-gradient(-90deg, #00A2A8, #04ECCA);'
+  gradiant_purple = 'linear-gradient(-90deg, #714FBE, #B15AA1);'
+  COLORS = [gradiant_red, gradiant_purple, gradiant_green, gradiant_blue]
   belongs_to :user
   has_many :bookings
 end
