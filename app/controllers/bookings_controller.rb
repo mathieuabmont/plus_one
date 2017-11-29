eaff48dad4099a51e242011b33505b1841539486
Sonialanglet <slanglet@loeildumarketing.com>
2017-11-29 15:28:39 +0100

    authorize after user attribution

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,9 @@ class BookingsController < ApplicationController
   def create
     @prestation = Prestation.find(params[:prestation_id])
     @booking = Booking.new(booking_params)
-    authorize @booking
     @booking.prestation = @prestation
     @booking.user = current_user
+    authorize @booking
     @booking.save
     redirect_to booking_path(@prestation)
   end
