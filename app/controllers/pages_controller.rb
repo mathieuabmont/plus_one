4137e550b3d280bad4197ee3e12bff7e3f64efb2
Alix de Bricourt <alix.debricourt@gmail.com>
2017-11-30 12:30:19 +0100

    Update of our cards on home, index & source

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :components]
 
   def home
-    @prestation = Prestation.all.sample
+    @prestations = Prestation.all.sample(3)
   end
 
   def components
