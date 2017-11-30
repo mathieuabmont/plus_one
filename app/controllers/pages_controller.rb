39b125da3acac117f77f2a981aab1df3094e38b0
Alix de Bricourt <alix.debricourt@gmail.com>
2017-11-30 12:41:16 +0100

    Home page phase 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :components]
 
   def home
-    @prestations = Prestation.all.sample(3)
+    @prestations = Prestation.all.sample(4)
   end
 
   def components
